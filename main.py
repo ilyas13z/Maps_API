26323928677ef98d637412bc33e230566f5bda8f
ilyas13z <ilyas13z@yandex.ru>
2023-04-29 22:49:22 +0500

    Большая задача по Maps API. Часть №4

--- main.py
+++ main.py
@@ -35,115 +35,30 @@ while running:
             if event.key == pygame.K_PAGEUP:
                 if 0 <= couns / 1.5 <= 90.0:
                     couns /= 1.5
-                    map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "spn": ','.join([str(couns), str(couns)]),
-                        "l": map_type
-                    }
-                    response = requests.get(map_api_server, params=map_params)
-                    if not response:
-                        print("Это край, дальше нельзя!")
-                    else:
-                        with open('map.png', "wb") as file:
-                            file.write(response.content)
-                        screen.blit(pygame.image.load(map_file), (0, 0))
-                        pygame.display.flip()
-                        os.remove(map_file)
             if event.key == pygame.K_PAGEDOWN:
                 if 0 <= couns * 1.5 <= 90.0:
                     couns *= 1.5
-                    map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "spn": ','.join([str(couns), str(couns)]),
-                        "l": map_type
-                    }
-                    response = requests.get(map_api_server, params=map_params)
-                    map_file = "map.png"
-                    if not response:
-                        print("Это край, дальше нельзя!")
-                    else:
-                        with open('map.png', "wb") as file:
-                            file.write(response.content)
-                        screen.blit(pygame.image.load(map_file), (0, 0))
-                        pygame.display.flip()
-                        os.remove(map_file)
             if event.key == pygame.K_DOWN:
                 if 80 >= float(toponym_lattitude) - couns * 2 >= -80:
                     if float(toponym_lattitude) - couns * 2 <= 80:
                         toponym_lattitude = str(float(toponym_lattitude) - couns * 2)
                     else:
                         toponym_lattitude = 90
-                    map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "spn": ','.join([str(couns), str(couns)]),
-                        "l": map_type
-                    }
-                    response = requests.get(map_api_server, params=map_params)
-                    if not response:
-                        print("Это край, дальше нельзя! K_DOWN")
-                    else:
-                        with open('map.png', "wb") as file:
-                            file.write(response.content)
-                        screen.blit(pygame.image.load(map_file), (0, 0))
-                        pygame.display.flip()
-                        os.remove(map_file)
                 else:
                     toponym_lattitude = '-80'
             if event.key == pygame.K_UP:
                 if 80 >= float(toponym_lattitude) + couns * 2 >= -80:
                     toponym_lattitude = str(float(toponym_lattitude) + couns * 2)
-                    map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "spn": ','.join([str(couns), str(couns)]),
-                        "l": map_type
-                    }
-                    response = requests.get(map_api_server, params=map_params)
-                    if not response:
-                        print("Это край, дальше нельзя! K_UP")
-                    else:
-                        with open('map.png', "wb") as file:
-                            file.write(response.content)
-                        screen.blit(pygame.image.load(map_file), (0, 0))
-                        pygame.display.flip()
-                        os.remove(map_file)
                 else:
                     toponym_lattitude = '80'
             if event.key == pygame.K_LEFT:
                 if 180 >= float(toponym_longitude) - couns * 2 >= -180:
                     toponym_longitude = str(float(toponym_longitude) - couns * 2)
-                    map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "spn": ','.join([str(couns), str(couns)]),
-                        "l": map_type
-                    }
-                    response = requests.get(map_api_server, params=map_params)
-                    if not response:
-                        print("Это край, дальше нельзя! K_LEFT")
-                    else:
-                        with open('map.png', "wb") as file:
-                            file.write(response.content)
-                        screen.blit(pygame.image.load(map_file), (0, 0))
-                        pygame.display.flip()
-                        os.remove(map_file)
                 else:
                     toponym_longitude = '-179'
             if event.key == pygame.K_RIGHT:
                 if 180 >= float(toponym_longitude) + couns * 2 >= -180:
                     toponym_longitude = str(float(toponym_longitude) + couns * 2)
-                    map_params = {
-                        "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                        "spn": ','.join([str(couns), str(couns)]),
-                        "l": "map"
-                    }
-                    response = requests.get(map_api_server, params=map_params)
-                    if not response:
-                        print("Это край, дальше нельзя! K_RIGHT")
-                    else:
-                        with open('map.png', "wb") as file:
-                            file.write(response.content)
-                        screen.blit(pygame.image.load(map_file), (0, 0))
-                        pygame.display.flip()
-                        os.remove(map_file)
                 else:
                     toponym_longitude = '179'
             if event.key == pygame.K_q:
@@ -152,6 +67,20 @@ while running:
                 map_type = 'sat'
             if event.key == pygame.K_e:
                 map_type = 'sat,skl'
+            map_params = {
+                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                "spn": ','.join([str(couns), str(couns)]),
+                "l": map_type
+            }
+            response = requests.get(map_api_server, params=map_params)
+            if not response:
+                print("Это край, дальше нельзя! K_RIGHT")
+            else:
+                with open('map.png', "wb") as file:
+                    file.write(response.content)
+                screen.blit(pygame.image.load(map_file), (0, 0))
+                pygame.display.flip()
+                os.remove(map_file)
     font = pygame.font.Font(None, 50)
     text = font.render("Переключение буквами Q W E", True, (100, 255, 100))
     screen.blit(text, (50, 450))
