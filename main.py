40e53fd72db7355f46c512ef90ea18ea49823827
AmirHusnutdinov <amir800900@mail.ru>
2023-04-29 18:57:58 +0500

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -25,43 +25,55 @@ while flag:
     except BaseException:
         pass
 
-# Долгота и широта:
-toponym_longitude, toponym_lattitude = coodrinates
-# Собираем параметры для запроса к StaticMapsAPI:
-map_params = {
-    "ll": f'{toponym_longitude},{toponym_lattitude}',
-    "z": z,
-    "l": "map"
-}
 
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(response.url)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-with open('map.png', "wb") as file:
-    file.write(response.content)
+def create_map(sc):
+    toponym_longitude, toponym_lattitude = coodrinates
+    map_params = {
+        "ll": f'{toponym_longitude},{toponym_lattitude}',
+        "z": sc,
+        "l": "map"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(response.url)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    with open('map.png', "wb") as file:
+        file.write(response.content)
+
+
+create_map(z)
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 running = True
+
 fps = 10
 clock = pygame.time.Clock()
+
 img = load_image('map.png')
+
 screen.blit(img, (0, 0))
 ind = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ind += 1
-            if event.key == pygame.K_LEFT:
-                ind -= 1
-    screen.blit(img, (0, 0))
 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == 1073741921:
+                if int(z) < 17:
+                    z = str(int(z) + 1)
+            if event.key == 1073741915:
+                if int(z) > 0:
+                    z = str(int(z) - 1)
+            create_map(z)
+            img = load_image('map.png')
+    screen.blit(img, (0, 0))
+    clock.tick(fps)
     pygame.display.flip()
+os.remove('map.png')
 pygame.quit()
