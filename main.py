3bea721786f0985098e5157615d384a5923aa280
ilyas13z <ilyas13z@yandex.ru>
2023-04-30 12:15:36 +0500

    Большая задача по Maps API. Часть №5

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import os
 import sys
-
+import pygame_textinput
 import pygame
 import requests as requests
+
+
+def get_coord_from_name(name):
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?" \
+                       f"apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={name}&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        json_response = response.json()
+        top = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        coord = top["Point"]["pos"]
+        coord = coord.split()
+        return coord[0], coord[1]
+    else:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+
+
+
 map_type = 'map'
 pygame.init()
+textinput = pygame_textinput.TextInputVisualizer()
 toponym_coodrinates = input('Введите координаты без пробелов с запятой (например 37.677751,55.757718): ').split(',')
 zennet = float(input('Введите масштаб: (в формате spn): '))
 dct_resp, lst_resp = {}, []
@@ -21,17 +41,30 @@ with open(map_file, "wb") as file:
     file.write(response.content)
 running = True
 couns = zennet
-screen = pygame.display.set_mode((600, 500))
+screen = pygame.display.set_mode((600, 700))
 screen.blit(pygame.image.load(map_file), (0, 0))
+clock = pygame.time.Clock()
 pygame.display.flip()
-os.remove(map_file)
+old_response = response.url
 while running:
-    for event in pygame.event.get():
+    pygame.draw.rect(screen, 'black', (0, 545, 600, 250))
+    events = pygame.event.get()
+
+    textinput.update(events)
+    textinput.font_color = 'white'
+    textinput.cursor_color = 'white'
+    screen.blit(textinput.surface, (10, 550))
+    pygame.display.update()
+    pygame.display.flip()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
+            pygame.display.flip()
+            if event.key == pygame.K_RETURN:
+                toponym_longitude, toponym_lattitude = get_coord_from_name(textinput.value)
             if event.key == pygame.K_PAGEUP:
                 if 0 <= couns / 1.5 <= 90.0:
                     couns /= 1.5
@@ -61,28 +94,36 @@ while running:
                     toponym_longitude = str(float(toponym_longitude) + couns * 2)
                 else:
                     toponym_longitude = '179'
-            if event.key == pygame.K_q:
+            if event.key == pygame.K_F1:
                 map_type = 'map'
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_F2:
                 map_type = 'sat'
-            if event.key == pygame.K_e:
+            if event.key == pygame.K_F3:
                 map_type = 'sat,skl'
             map_params = {
                 "ll": ",".join([toponym_longitude, toponym_lattitude]),
                 "spn": ','.join([str(couns), str(couns)]),
-                "l": map_type
+                "l": map_type,
+                "pt": ','.join([toponym_longitude, toponym_lattitude, 'pm2rdm'])
             }
             response = requests.get(map_api_server, params=map_params)
             if not response:
-                print("Это край, дальше нельзя! K_RIGHT")
+                print("Это край, дальше нельзя!")
+            elif response.url == old_response:
+                pass
             else:
                 with open('map.png', "wb") as file:
                     file.write(response.content)
-                screen.blit(pygame.image.load(map_file), (0, 0))
-                pygame.display.flip()
-                os.remove(map_file)
-    font = pygame.font.Font(None, 50)
-    text = font.render("Переключение буквами Q W E", True, (100, 255, 100))
-    screen.blit(text, (50, 450))
+                old_response = response.url
+            screen.blit(pygame.image.load(map_file), (0, 0))
+    font = pygame.font.Font(None, 40)
+    font1 = pygame.font.Font(None, 30)
+    text1 = font.render("Переключение клавишами F1 F2 F3", True, (100, 255, 100))
+    text2 = font1.render("Это поисковое поле:", True, (100, 255, 100))
+    text3 = font1.render("(чтобы увидеть результат нажмите enter)", True, (100, 255, 100))
+    screen.blit(text1, (50, 450))
+    screen.blit(text2, (0, 480))
+    screen.blit(text3, (0, 515))
     pygame.display.flip()
+    clock.tick(60)
 sys.exit()
